5ae1300f7a4e52073191095bdb27af46c2452138
Bhautik <bhautikkapadiya06@gmail.com>
2025-08-18 22:47:58 +0530

    feat(doctor): adding the doctor controller and field in the doctor management system

---
+++ Doctor-Patient-System/Doctor-Patient-System/Migrations/20250817141225_intital commit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctor_Patient_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class intitalcommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
