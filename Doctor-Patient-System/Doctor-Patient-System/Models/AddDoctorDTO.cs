8712bfe7954bc0d5d5c876c505dfba5f29474869
Bhautik <bhautikkapadiya06@gmail.com>
2025-08-25 23:23:20 +0530

    feat(doctor): change the field name and adding new feature of doctor in the project

--- Doctor-Patient-System/Doctor-Patient-System/Models/AddDoctorDTO.cs
+++ Doctor-Patient-System/Doctor-Patient-System/Models/AddDoctorDTO.cs
@@ -3,7 +3,7 @@
     public class AddDoctorDTO
     {
         public int DoctorId { get; set; }
-        public required string Name { get; set; }
+        public required string DName { get; set; }
         public required string Specialization { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
--- Doctor-Patient-System/Doctor-Patient-System/Models/Entities/Patient.cs
+++ Doctor-Patient-System/Doctor-Patient-System/Models/Entities/Patient.cs
@@ -5,9 +5,10 @@ namespace Doctor_Patient_System.Models.Entities
     public class Patient
     {
         public required int PatientId { get; set; }
-        public required string Name { get; set; }
+        public required string PName { get; set; }
         public required DateOnly DOB { get; set; }
         public string ? Number { get; set; }
+        public int DoctorId { get; set; }
 
     }
 }
